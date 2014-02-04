d2d5886728c3c94ffc78c72930f95931d050a29c
Nam <nam@propagate.com.au>
2014-02-04 18:01:02 +1100

    update job create

--- application/modules/job/views/create.php
+++ application/modules/job/views/create.php
@@ -1,7 +1,7 @@
 <div class="col-md-12">
 	<div class="box top-box">
         <h2>Create Jobs</h2>
-        <p>To start creating jobs select a client and a campaign name. A campaign name is used for you to find the shifts you create later. A job campaign can contain many shifts across many days, weeks or months.</p>
+        <p>To start creating jobs select a client and a campaign name. A campaign name is used for you to find the shifts you create later. A job campaign can contain many shifts across many days, weeks or months.</p>
     </div>
 </div>
 
@@ -9,26 +9,26 @@
 	<div class="box bottom-box">
     	<div class="inner-box">
             <h2>Create Job - Step 1</h2>
-            <p>Choose a client and enter a camapign name to start creating jobs. Client departments can be set up when you create clients, 
-            a client will be able to filterjobs assosicated to them by the cleitn department when they login to their client account.
-            </p>
+            <p>Choose a client and enter a campaign name to start creating jobs. Client departments can be set up when you create clients, 
+a client will be able to filter jobs associated to them by the client department when they login to their client account.</p>
         
-            <!--<a href="<?=base_url();?>job/search"><i class="fa fa-search"></i> Search Jobs</a>-->
-        
-                
             <form class="form-horizontal" role="form" method="post" action="<?=base_url();?>job/create">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <div class="form-group<?=form_error('name')? ' has-error' : '';?>">
-                            <label for="name" class="col-lg-4 control-label">Job Group Name</label>
-                            <div class="col-lg-8">
-                                <input type="text" class="form-control" name="name" id="name" value="<?=set_value('name');?>" />
+                            <label for="name" class="col-lg-2 control-label">Campaign Name</label>
+                            <div class="col-lg-4">
+                                <input type="text" class="form-control" name="name" id="name" placeholder="" value="<?=set_value('name');?>" />
                             </div>
+                            <div class="col-lg-6"><span class="help-block">Enter a unique campaign name that you will be able to search for later</span></div>
                         </div>
                         <div class="form-group<?=form_error('client_id')? ' has-error' : '';?>">
-                            <label for="client_id" class="col-lg-4 control-label">Client</label>
-                            <div class="col-lg-8">
+                            <label for="client_id" class="col-lg-2 control-label">Client</label>
+                            <div class="col-lg-4">
                                 <?=modules::run('client/dropdown', 'client_id', set_value('client_id'));?>
+                            </div>
+                            <div class="col-lg-4">
+                            	<span class="help-block"><a><i class="fa fa-plus"></i></a> &nbsp; <a href="#">Create New Client</a></span>
                             </div>
                         </div>
                     </div>            
@@ -37,7 +37,7 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <div class="col-lg-offset-4 col-lg-8">
-                                <button type="submit" class="btn btn-info"><i class="fa fa-plus"></i> Create Job</button>
+                                <button type="submit" class="btn btn-core"><i class="fa fa-plus"></i> Create Job</button>
                             </div>
                         </div>
                     </div>
