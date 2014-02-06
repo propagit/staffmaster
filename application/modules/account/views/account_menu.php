5de583acbea1238e62f0eee10855a97495f9556e
Nam <nam@propagate.com.au>
2014-02-07 10:18:35 +1100

    update user menu

---
+++ application/modules/account/views/account_menu.php
@@ -0,0 +1,18 @@
+<div class="avatar pull">
+    <img src="<?=base_url();?>assets/img/dummy/default-avatar.png" title="User Avatar" alt="user-avatar.jpg" />
+</div>
+<ul>
+    <li><a title="Dashboard" href=""><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li><li class="li-seprator">|</li>
+    <li><a title="Logout" href="<?=base_url();?>logout"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li>
+    <li><a title="Message" href=""><i class="fa fa-comments"></i> <span>Message</span></a></li><li class="li-seprator">|</li>
+    <? if ($this->session->userdata('force_staff')) { ?>
+    <li><a title="Staff Account" href="<?=base_url();?>account/admin"><i class="fa fa-user"></i> <span>Admin Account</span></a></li>
+    <? } else { ?>
+    <li><a title="Staff Account" href="<?=base_url();?>account/staff"><i class="fa fa-user"></i> <span>Staff Account</span></a></li>
+    <? } ?>
+</ul>
+<div class="message-badge">
+    <span class="badge badge-xs danger">1</span>
+    <i class="fa fa-caret-right"></i>
+    <i class="fa fa-caret-down"></i>
+</div>
