b7bda01a064c94767305332e923dbdd939132bf7
Raquel <raquel@propagate.com.au>
2014-02-12 13:01:43 +1100

    Commit availbility staff

---
+++ application/modules/staff/views/edit_availability.php
@@ -0,0 +1,25 @@
+<br />
+<h2> Your Availability </h2>
+<p> Please let us know the times that you are available for work</p>
+
+<div id="list_availability"></div>
+
+
+
+<script>
+$(function(){
+	load_availability(<?=$staff['user_id']?>);
+});
+function load_availability(user_id)
+{
+	preloading($('#list_availability'));
+	$.ajax({
+		type: "POST",
+		url: "<?=base_url();?>staff/ajax/load_availability",
+		data: {user_id: user_id},
+		success: function(html) {			
+			loaded($('#list_availability'), html);
+		}
+	})
+}
+</script>
