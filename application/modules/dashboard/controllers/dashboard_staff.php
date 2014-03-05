37b105e57160dcb11e7287704eda162fbf474571
Raquel <raquel@propagate.com.au>
2014-03-05 16:43:07 +1100

    staff dashboard

--- application/modules/dashboard/controllers/dashboard_staff.php
+++ application/modules/dashboard/controllers/dashboard_staff.php
@@ -10,15 +10,23 @@ class Dashboard_staff extends MX_Controller {
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->model('setting/setting_model');
 	}
 	
 	public function index()
 	{
+		$company = $this->setting_model->get_profile();
+		$data['company'] = $company;
 		$this->load->view('staff/dashboard', isset($data) ? $data : NULL);
 	}
 	
 	public function activity_log()
 	{
 		$this->load->view('staff/activity_log', isset($data) ? $data : NULL);
+	}
+	
+	public function conversation()
+	{
+		$this->load->view('staff/conversation', isset($data) ? $data : NULL);
 	}
 }
