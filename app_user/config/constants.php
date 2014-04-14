01994f021e8e818292cdf72320485289b031b408
Kaushtuv <kaushtuv@propagate.com.au>
2014-04-14 12:50:57 +1000

    fix constant

--- app_user/config/constants.php
+++ app_user/config/constants.php
@@ -16,10 +16,7 @@ include('./../app_public/config/constants.php');
 | always be used to set the mode correctly.
 |
 */
-define('FILE_READ_MODE', 0644);
-define('FILE_WRITE_MODE', 0666);
-define('DIR_READ_MODE', 0755);
-define('DIR_WRITE_MODE', 0777);
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,14 +27,7 @@ define('DIR_WRITE_MODE', 0777);
 |
 */
 
-define('FOPEN_READ',							'rb');
-define('FOPEN_READ_WRITE',						'r+b');
-define('FOPEN_WRITE_CREATE_DESTRUCTIVE',		'wb'); // truncates existing file data, use with care
-define('FOPEN_READ_WRITE_CREATE_DESTRUCTIVE',	'w+b'); // truncates existing file data, use with care
-define('FOPEN_WRITE_CREATE',					'ab');
-define('FOPEN_READ_WRITE_CREATE',				'a+b');
-define('FOPEN_WRITE_CREATE_STRICT',				'xb');
-define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
+
 
 
 define('COLOR_PRIM', '#1868b1');
