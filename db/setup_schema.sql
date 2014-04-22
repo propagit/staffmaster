ab5066a84f5fd5b65a73d9a241cf6bbc0ec29084
Nam Nguyen <nam@propagate.com.au>
2014-04-22 16:23:40 +1000

    update setup db

--- db/setup_schema.sql
+++ db/setup_schema.sql
@@ -1162,6 +1162,16 @@ CREATE TABLE IF NOT EXISTS `job_shift_client_request` (
 
 -- --------------------------------------------------------
 
+CREATE TABLE IF NOT EXISTS `job_shift_notes` (
+  `job_shift_note_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `shift_id` bigint(20) NOT NULL,
+  `note` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `added_by_user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`job_shift_note_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
 --
 -- Table structure for table `job_shift_staff_apply`
 --
