3dd118f7b07ef589a932f0193090b6eea921ae6a
Nam Nguyen <nam@propagate.com.au>
2014-06-05 18:25:56 +1000

    test sms

--- app_user/modules/sms/controllers/sms.php
+++ app_user/modules/sms/controllers/sms.php
@@ -8,6 +8,12 @@ class Sms extends MX_Controller {
 		$this->load->model('Cbf_model');
 	}
 	
+	function test()
+	{
+		$a = $this->cbf_model->test();
+		var_dump($a);
+	}
+	
 	function msg_form_view($selected_module_ids) {
 		$data['selected_module_ids'] = json_decode($selected_module_ids);
 		$this->load->view('msg_form_view', isset($data) ? $data : NULL);
