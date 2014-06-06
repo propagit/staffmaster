f418e8834042ecfaf17464570e11e3b73d7ae548
Nam Nguyen <nam@propagate.com.au>
2014-06-06 10:13:31 +1000

    php info

--- app_user/modules/sms/controllers/sms.php
+++ app_user/modules/sms/controllers/sms.php
@@ -8,15 +8,26 @@ class Sms extends MX_Controller {
 		$this->load->model('cbf_model');
 	}
 	
+	public function index($method='', $param='')
+	{
+		switch($method)
+		{
+			case 'test':
+					$this->test();
+				break;
+			case 'info':
+					phpinfo();
+				break;
+			default:					
+				break;
+		}
+		
+	}
+	
 	function test()
 	{
 		$a = $this->cbf_model->test();
 		var_dump($a);
-	}
-	
-	function info()
-	{
-		phpinfo();
 	}
 	
 	function msg_form_view($selected_module_ids) {
