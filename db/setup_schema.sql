7ee8e11f108977b9975f25483ef7d728bcd5be86
Nam Nguyen <nam@propagate.com.au>
2014-07-14 14:10:00 +1000

    update sms schema

--- db/setup_schema.sql
+++ db/setup_schema.sql
@@ -1664,6 +1664,25 @@ CREATE TABLE IF NOT EXISTS `shift_brief` (
 
 -- --------------------------------------------------------
 
+
+CREATE TABLE `sms_templates` (
+  `template_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `msg` text NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`template_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `sms_templates`
+--
+
+INSERT INTO `sms_templates` (`template_id`, `title`, `msg`, `status`) VALUES
+(1, 'Work Request', 'Dear {FirstName}. Can you work as a {Role} from {StartTime} to {FinishTime}, on {Date} at {Venue}. Reply Y{Code} for yes or N{Code} for no. {CompanyName}', 1),
+(2, 'Work Confirmation', 'You have been confirmed to work on {Date} at {StartTime} please login to your account for full details.', 1),
+(3, 'Invalid Code', 'The code {Code} is no longer valid. The position may have been filled or cancelled. Please contact us for more information', 1);
+
+
 --
 -- Table structure for table `staff_custom_fields`
 --
