74864e63b78d7fb5c89fbbd3428c2ce59df30116
Nam Nguyen <nam@propagate.com.au>
2014-07-15 14:59:30 +1000

    fix apply work table

--- app_user/modules/work/views/staff/day_shifts_table_view.php
+++ app_user/modules/work/views/staff/day_shifts_table_view.php
@@ -9,7 +9,7 @@
 			<th class="center">Start</th>
 			<th class="center">Finish</th>
 			<th class="center">Break</th>
-			<th class="center">Pay</th>
+			<th class="center">Pay Rate</th>
 			<th class="center">Brief</th>
 			<th class="center" width="100">Apply</th>
 		</tr>
@@ -18,8 +18,8 @@
 	<? foreach($shifts as $shift) { ?>
 	<tr>
 		<td class="wp-date"><input type="checkbox" class="select_shift" value="<?=$shift['shift_id'];?>" /></td>
-		<td><? $client = modules::run('client/get_client', $shift['client_id']); echo $client['company_name']; ?></td>
-		<td><?=modules::run('attribute/role/display_role', $shift['role_id']);?></td>
+		<td width="180"><? $client = modules::run('client/get_client', $shift['client_id']); echo $client['company_name']; ?></td>
+		<td width="180"><?=modules::run('attribute/role/display_role', $shift['role_id']);?></td>
 		<td>
 			<? if ($shift['venue_id']) { ?>			
 			<a data-toggle="modal" data-target=".bs-modal-sml" href="<?=base_url();?>common/ajax/load_venue_map/<?=$shift['venue_id'];?>"><i class="fa fa-map-marker"></i> &nbsp; <?=modules::run('attribute/venue/display_venue', $shift['venue_id']);?></a>			
@@ -27,12 +27,12 @@
 			Not Specified
 			<? } ?>
 		</td>
-		<td class="center"><?=date('H:i', $shift['start_time']);?></td>
-		<td class="center"><?=date('H:i', $shift['finish_time']);?> <?=(date('d', $shift['finish_time']) != date('d', $shift['start_time'])) ? '<span class="error">*</span>': '';?></td>
-		<td class="center"><?=modules::run('common/break_time', $shift['break_time']);?></td>
-		<td class="center"></td>
-		<td class="center"><a target="_blank" href="<?=base_url();?>brief/view_brief/<?=$shift['shift_id'];?>"><i class="fa fa-eye"></i></a></td>
-		<td class="center">
+		<td class="center" width="100"><?=date('H:i', $shift['start_time']);?></td>
+		<td class="center" width="100"><?=date('H:i', $shift['finish_time']);?> <?=(date('d', $shift['finish_time']) != date('d', $shift['start_time'])) ? '<span class="error">*</span>': '';?></td>
+		<td class="center" width="80"><?=modules::run('common/break_time', $shift['break_time']);?></td>
+		<td class="center" width="150"><?=modules::run('attribute/payrate/display_payrate', $shift['payrate_id']);?></td>
+		<td class="center" width="50"><a target="_blank" href="<?=base_url();?>brief/view_brief/<?=$shift['shift_id'];?>"><i class="fa fa-eye"></i></a></td>
+		<td class="center" width="60">
 			<? 
 			if(modules::run('work/ajax/is_shift_applied', $shift['shift_id'])) { ?>			
 			<a class="unapply_shift_<?=$date;?> btn btn-xs btn-success" data-shift-id="<?=$shift['shift_id'];?>"><i class="fa fa-thumbs-o-up"></i> Applied</a>
