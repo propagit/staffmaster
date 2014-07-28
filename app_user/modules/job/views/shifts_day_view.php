dd1fadf899c81125a547b0b6b97346e81b67900a
Nam Nguyen <nam@propagate.com.au>
2014-07-28 15:08:28 +1000

    fix contact staff

--- app_user/modules/job/views/shifts_day_view.php
+++ app_user/modules/job/views/shifts_day_view.php
@@ -269,7 +269,7 @@ $(function(){
 			  url: "<?=base_url();?>email/ajax/get_send_email_modal",
 			  data: $('#apply-shift-contact-email-form').serialize(),
 			  success: function(html) {
-			  	//alert(html);
+			  	alert(html);
 				  $('#ajax-email-apply-shift-modal').html(html);
 				  $('#email-modal').modal('show');	
 			  }
