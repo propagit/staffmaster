90c612a66273f85d95d053a73b29d5be9ee0e3a6
Nam Nguyen <nam@propagate.com.au>
2014-07-30 10:43:34 +1000

    update breakdown

--- app_user/modules/invoice/views/create/breakdown.php
+++ app_user/modules/invoice/views/create/breakdown.php
@@ -4,7 +4,8 @@
 			<? foreach($items as $item) { 
 			if ($item['include_timesheets']) {
 			$job = modules::run('job/get_job', $item['job_id']);
-			$timesheets = modules::run('invoice/get_job_timesheets', $item['job_id'], INVOICE_READY);
+			#$timesheets = modules::run('invoice/get_job_timesheets', $item['job_id'], INVOICE_READY);
+			$timesheets = modules::run('invoice/get_invoice_timesheets', $invoice['invoice_id']);
 			 ?>
 			 
 			<? if ($job) { ?>
