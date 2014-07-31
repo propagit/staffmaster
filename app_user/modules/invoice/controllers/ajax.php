09b3ab3dd34dc5e14f43fb876de0cc75f6ff5d1f
Kaushtuv <kaushtuvgurung@gmail.com>
2014-07-31 11:23:54 +1000

    invoice double up bug fix

--- app_user/modules/invoice/controllers/ajax.php
+++ app_user/modules/invoice/controllers/ajax.php
@@ -846,7 +846,7 @@ class Ajax extends MX_Controller {
 					if ($item['include_timesheets']) # Item that include timesheets
 					{
 						#$timesheets = modules::run('invoice/get_job_timesheets', $item['job_id'], INVOICE_READY);
-						$timesheets = modules::run('invoice/get_invoice_timesheets', $invoice_id);
+						$timesheets = modules::run('invoice/get_invoice_timesheets', $invoice_id, $item['job_id']);
 						if (count($timesheets) > 0) {
 							foreach($timesheets as $timesheet) 
 							{
