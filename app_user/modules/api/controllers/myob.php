ec71aaa3d43c8969152b67d1daf3ac1a6e0ce59b
Nam Nguyen <nam@propagate.com.au>
2014-08-13 15:02:00 +1000

    update sync myob

--- app_user/modules/api/controllers/myob.php
+++ app_user/modules/api/controllers/myob.php
@@ -655,8 +655,8 @@ class Myob extends MX_Controller {
 				'Lines' => $lines
 			);
 			
-			var_dump($timesheet_data);
-			/*
+			#var_dump($timesheet_data);
+			
 			$params = json_encode($timesheet_data);
 			
 			$cftoken = base64_encode('Administrator:');
@@ -670,7 +670,6 @@ class Myob extends MX_Controller {
 			);
 			
 			$url = $this->cloud_api_url . $this->company_id . '/Payroll/Timesheet/' . $employee->UID;
-			#var_dump($url); die();
 			
 			$ch = curl_init($url); 
 			
@@ -684,8 +683,7 @@ class Myob extends MX_Controller {
 			
 			
 			$response = curl_exec($ch);
-			curl_close($ch); */
-			
+			curl_close($ch); 			
 		}
 		
 	}
