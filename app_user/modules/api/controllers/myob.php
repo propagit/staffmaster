a02a6b004a08a9ee0b2b65e100a7a2f378c5a6bf
Nam Nguyen <nam@propagate.com.au>
2014-08-18 16:31:29 +1000

    update

--- app_user/modules/api/controllers/myob.php
+++ app_user/modules/api/controllers/myob.php
@@ -162,7 +162,7 @@ class Myob extends MX_Controller {
 		$response = curl_exec($ch); 
 		curl_close($ch);
 		$company = json_decode($response);
-		var_dump($company); die();
+		#var_dump($company); die();
 		if (isset($company[0]))
 		{
 			$this->config_model->add(array('key' => 'myob_company_id', 'value' => $company[0]->Id));
