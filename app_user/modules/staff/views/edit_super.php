f9c20504d2101ac2354e8e2492a16439b4614247
Nam Nguyen <namnguyen@Nams-MBP.gateway>
2014-11-07 11:08:07 +1100

    update super

--- app_user/modules/staff/views/edit_super.php
+++ app_user/modules/staff/views/edit_super.php
@@ -19,11 +19,11 @@
 	</div>
 </div>
 <? $platform = $this->config_model->get('accounting_platform');
-if ($platform == 'myob') { 
+if ($platform == 'myob') {
 	$id = modules::run('setting/superinformasi', 'super_fund_external_id');
 	if ($id) {
 		$super_fund = modules::run('api/myob/connect', 'read_super_fund~' . $id);
-	}	
+	}
 ?>
 <div class="row" id="employer_choice">
 	<? if (isset($super_fund)) { ?>
@@ -45,7 +45,7 @@ if ($platform == 'myob') {
 	<? } else { ?>
 	<div class="form-group">
 		<div class="col-md-10 col-md-offset-2">
-			No company super fund is currently set up. 
+			No company super fund is currently set up.
 			<? if(!modules::run('auth/is_staff')){ ?>
 			<br />To set up a default company super fund go to "System Settings" > "Company Profile"
 			<? } ?>
@@ -68,7 +68,7 @@ if ($platform == 'myob') {
 </div>
 
 <div class="row">
-	<div class="form-group">		
+	<div class="form-group">
 		<label for="s_employee_id" class="col-md-2 control-label">Staff Membership Number</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_employee_id" name="s_employee_id" value="<?=$staff['s_employee_id'];?>" />
@@ -89,10 +89,10 @@ if ($platform == 'myob') {
 		</div>
 	</div>
 
-	
+
 	<div class="form-group<? //if(modules::run('common/check_super', $staff['s_fund_name'])) { echo ' has-success'; } else { echo '  has-warning'; } ?>">
 		<label for="s_fund_name" class="col-md-2 control-label">Super Fund Name</label>
-		<div class="col-md-4">			
+		<div class="col-md-4">
             <?=modules::run('common/dropdown_supers', 's_fund_name',($staff['s_choice'] == 'own') ? $staff['s_fund_name'] : modules::run('setting/superinformasi', 'super_fund_name',''));?>
 
 		</div>
@@ -103,7 +103,7 @@ if ($platform == 'myob') {
 </div>
 
 
-<div class="row" id="employer_choice">
+<!-- <div class="row" id="employer_choice">
 	<div class="form-group">
 		<label for="s_product_id" class="col-md-2 control-label">Super Product ID</label>
 		<div class="col-md-4">
@@ -114,48 +114,48 @@ if ($platform == 'myob') {
 			<input type="text" class="form-control" id="s_fund_website" name="s_fund_website" value="<?=modules::run('setting/superinformasi', 'super_fund_website','');?>" readonly />
 		</div>
 	</div>
-		
+
 	<div class="form-group">
 		<label for="s_fund_phone" class="col-md-2 control-label">Super Phone</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_fund_phone" name="s_fund_phone" value="<?=modules::run('setting/superinformasi', 'super_fund_phone','');?>" readonly />
 		</div>
-	</div>						
-</div>
-					
-<div class="row" id="own_choice">
+	</div>
+</div> -->
+
+<!-- <div class="row" id="own_choice">
 	<div class="form-group">
 		<label for="s_membership" class="col-md-2 control-label">Membership Number</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_membership" name="s_membership" value="<?=$staff['s_membership'];?>" />
 		</div>
-		
+
 		<label for="s_fund_suburb" class="col-md-2 control-label">Suburb</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_fund_suburb" name="s_fund_suburb" value="<?=$staff['s_fund_suburb'];?>" />
 		</div>
-	</div>	
-	
+	</div>
+
 	<div class="form-group">
 		<label for="s_fund_address" class="col-md-2 control-label">Super Fund Address</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_fund_address" name="s_fund_address" value="<?=$staff['s_fund_address'];?>" />
 		</div>
-		
+
 		<label for="s_fund_website" class="col-md-2 control-label">State</label>
 		<div class="col-md-4">
 			<?=modules::run('common/field_select_states', 's_fund_state', $staff['s_fund_state']);?>
 		</div>
 	</div>
-	
+
 	<div class="form-group">
 		<label for="s_fund_postcode" class="col-md-2 control-label">Postcode</label>
 		<div class="col-md-4">
 			<input type="text" class="form-control" id="s_fund_postcode" name="s_fund_postcode" value="<?=$staff['s_fund_postcode'];?>" />
 		</div>
 	</div>
-</div>
-<? } ?>				
+</div> -->
+<? } ?>
 <br />
 <div class="row">
 	<div class="form-group">
@@ -165,7 +165,7 @@ if ($platform == 'myob') {
 			<li>A letter from the trustee stating that this is a complying fund or retirement savings account (RSA) or, for a self managed superannuation fund, a copy of documentation from the ATO confirming the fund is regulated</li>
 			<li>Written evidence from the fund stating that they will accept contributions from my employer, and</li>
 			<li>Details about how my employer can make contributions to this fund.</li>
-		</ol>					
+		</ol>
 		</div>
 	</div>
 </div>
@@ -176,7 +176,7 @@ if ($platform == 'myob') {
 				<input type="checkbox" name="s_agree" value="1"<?=($staff['s_agree']) ? ' checked' : '';?> /> I have read and agreed to the above statements.
             </div>
 		</div>
-	</div>					
+	</div>
 </div>
 <div class="row">
 	<div class="form-group">
@@ -189,11 +189,11 @@ if ($platform == 'myob') {
 <script>
 $(function(){
 	load_s_choice();
-	
+
 	$('input[name="s_choice"]').change(function(){
 		load_s_choice();
 	});
-	
+
 	$('#btn_update_super').click(function(){
 		var btn = $(this);
 		btn.button('loading');
@@ -213,21 +213,21 @@ $(function(){
 })
 function load_s_choice()
 {
-	var employee_super_name = '<?=modules::run('setting/superinformasi', 'super_fund_name','')?>'; 
+	var employee_super_name = '<?=modules::run('setting/superinformasi', 'super_fund_name','')?>';
 	var s_choice = $('input[name="s_choice"]:checked').val();
 	if (s_choice == "own") {
-		$('#own_choice').show();
+		//$('#own_choice').show();
 		$('input[name="s_fund_name"]').prop('readonly',false);
 		$('#own').prop('checked',true);
-		$('#employer_choice').hide();
+		//$('#employer_choice').hide();
 		//$('input[name="s_fund_name"]').val('')
 	}
 	else
-	{		
-		$('#employer_choice').show();
+	{
+		//$('#employer_choice').show();
 		$('input[name="s_fund_name"]').attr('readonly','readonly');
 		$('#employer').prop('checked',true);
-		$('#own_choice').hide();
+		//$('#own_choice').hide();
 		$('input[name="s_fund_name"]').val(employee_super_name);
 	}
 }
