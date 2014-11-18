cfcd761bd99b97164d9f3f65cb9d2857cf057cf0
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2014-11-19 10:03:33 +1100

    update menu

--- app_user/modules/account/views/account_menu.php
+++ app_user/modules/account/views/account_menu.php
@@ -10,11 +10,11 @@
     <li><a title="Dashboard" href="<?=base_url();?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li><!--<li class="li-seprator">|</li>-->
     <!-- <li><a title="Logout" href="<?=base_url();?>logout"><i class="fa fa-sign-out"></i> <span>Logout</span></a></li> -->
     <!-- <li><a title="Message" href=""><i class="fa fa-comments"></i> <span>Message</span></a></li><li class="li-seprator">|</li> -->
-    
+
     <? if ($this->session->userdata('force_staff')) { ?>
     <li><a title="Staff Account" href="<?=base_url();?>auth/admin"><i class="fa fa-user"></i> <span>Admin Account</span></a></li>
     <? } else { ?>
-    <? if ($user_data['is_admin']) { ?>
+    <? if ($user_data['is_staff']) { ?>
     <li><a title="Staff Account" href="<?=base_url();?>auth/staff"><i class="fa fa-user"></i> <span>Staff Account</span></a></li>
     <?php }else{ ?>
     <li><a title="Your Profile" href="<?=base_url();?>staff"><i class="fa fa-user"></i> <span>Your Profile</span></a></li>
