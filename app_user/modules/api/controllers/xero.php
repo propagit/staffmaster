c29d8adf7f8be0fd4cb343ed4d413aaac8d6587d
Nam Nguyen <namnguyen@192-168-1-13.tpgi.com.au>
2015-03-22 09:58:15 +1100

    test xero

--- app_user/modules/api/controllers/xero.php
+++ app_user/modules/api/controllers/xero.php
@@ -231,7 +231,7 @@ class Xero extends MX_Controller {
                 </Employees>";
         // var_dump($xml); die();
         $response = $this->XeroOAuth->request('PUT', $this->XeroOAuth->url('Employees', 'payroll'), array(), $xml);
-
+        var_dump($response);
         if ($this->XeroOAuth->response['code'] == 200) {
             $employees = $this->XeroOAuth->parseResponse($this->XeroOAuth->response['response'], $this->XeroOAuth->response['format']);
 
