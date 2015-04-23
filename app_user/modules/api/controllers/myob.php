0740c37de54ffde43c5323c9f4a5fdeea95742b7
Kaushtuv <kaushtuvgurung@gmail.com>
2015-04-23 14:18:49 +1000

    test myob

--- app_user/modules/api/controllers/myob.php
+++ app_user/modules/api/controllers/myob.php
@@ -453,7 +453,7 @@ class Myob extends MX_Controller {
 		);
 		#$filter = "filter=substringof('". $external_id ."',%20DisplayID)%20eq%20true";
 		#$filter = "filter=UID%20eq%20'". $uid ."'";
-		$filter = "UID%20eq%20'". $uid ."'";
+		$filter = "UID%20eq%20guid'". $uid ."'";
 		$url = $this->cloud_api_url . $this->company_id . '/Contact/Employee/?$' . $filter;
 
 		$ch = curl_init($url);
@@ -485,7 +485,7 @@ class Myob extends MX_Controller {
 	        'x-myobapi-version: v2'
 		);
 		#$filter = "filter=substringof('". $external_id ."',%20DisplayID)%20eq%20true";
-		$filter = "filter=UID%20eq%20'". $uid ."'";
+		$filter = "filter=UID%20eq%20guid'". $uid ."'";
 		#$filter = "UID%20eq%20'". $uid ."'";
 		$url = $this->cloud_api_url . $this->company_id . '/Contact/Employee/?$' . $filter;
 
