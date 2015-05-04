f1e0f98da39de2fde8a458b328f05cf4cc4a7915
Kaushtuv <kaushtuvgurung@gmail.com>
2015-05-04 21:25:37 +1000

    Notes Update

--- db/create_notes.sql
+++ db/create_notes.sql
@@ -1,8 +1,1 @@
-CREATE TABLE `user_notes` (
-`user_note_id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  `added_by` bigint(20) NOT NULL COMMENT 'user id of the admin who added the note',
-  `note` text NOT NULL,
-  `created_date` date NOT NULL,
-  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE  `user_notes` ADD PRIMARY KEY (  `user_note_id` )
