41cb2ae6a03eb98cada79db75da8559125f2563c
Kaushtuv <kaushtuvgurung@gmail.com>
2015-06-14 12:44:23 +1000

    Schema

--- db/setup_schema.sql
+++ db/setup_schema.sql
@@ -2063,13 +2063,14 @@ INSERT INTO `sms_templates` (`template_id`, `title`, `msg`, `status`) VALUES
 -- Table structure for table `staff_custom_fields`
 --
 
-CREATE TABLE `staff_custom_fields` (
+CREATE TABLE IF NOT EXISTS `staff_custom_fields` (
   `staff_custom_field_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) NOT NULL,
   `field_id` bigint(20) NOT NULL,
   `value` text NOT NULL,
+  `field_date` date NOT NULL,
   PRIMARY KEY (`staff_custom_field_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 -- --------------------------------------------------------
