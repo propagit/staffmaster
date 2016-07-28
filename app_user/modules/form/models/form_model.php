ddab8842c4f40588d4d94fcf7e6c3089327541b8
Nam Nguyen <namnd86@gmail.com>
2016-07-28 12:24:24 +1000

    update custom field order

--- app_user/modules/form/models/form_model.php
+++ app_user/modules/form/models/form_model.php
@@ -1,47 +1,47 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Form_model extends CI_Model {
-	
+
 	function add_form($data) {
 		$this->db->insert('forms', $data);
 		return $this->db->insert_id();
 	}
-	
+
 	function get_forms() {
 		$this->db->where('status > ', DELETED);
 		$query = $this->db->get('forms');
 		return $query->result_array();
 	}
-	
+
 	function get_form($form_id) {
 		$this->db->where('status > ', DELETED);
 		$this->db->where('form_id', $form_id);
 		$query = $this->db->get('forms');
 		return $query->first_row('array');
 	}
-	
+
 	function update_form($form_id, $data) {
 		$this->db->where('form_id', $form_id);
 		return $this->db->update('forms', $data);
 	}
-	
+
 	function delete_form($form_id) {
 		$this->db->where('form_id', $form_id);
 		return $this->db->update('forms', array('status' => DELETED));
 	}
-	
+
 	function get_custom_fields($form_id, $show_actived=false) {
 		$sql = "SELECT c.*, f.form_field_id, f.required
 				FROM custom_fields c
 					LEFT JOIN form_fields f ON (f.name = c.field_id AND f.form_id = $form_id)
-				WHERE c.admin_only = 0";
+				WHERE c.admin_only = 0 ORDER BY c.field_order";
 		if ($show_actived) {
 			$sql .= " AND f.form_field_id > 0";
 		}
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
-	
+
 	function active_field($form_id, $label, $name) {
 		$field = $this->get_field($form_id, $name);
 		if ($field) { # Found the field
@@ -65,7 +65,7 @@ class Form_model extends CI_Model {
 			return $this->db->insert_id();
 		}
 	}
-	
+
 	function require_field($form_id, $name) {
 		$field = $this->get_field($form_id, $name);
 		if ($field) {
@@ -80,7 +80,7 @@ class Form_model extends CI_Model {
 			return 0;
 		}
 	}
-	
+
 	function get_fields($form_id, $active = INACTIVE) {
 		if ($active) {
 			$this->db->where('active', ACTIVE);
@@ -89,14 +89,14 @@ class Form_model extends CI_Model {
 		$query = $this->db->get('form_fields');
 		return $query->result_array();
 	}
-	
+
 	function get_field($form_id, $name) {
 		$this->db->where('form_id', $form_id);
 		$this->db->where('name', $name);
 		$query = $this->db->get('form_fields');
 		return $query->first_row('array');
 	}
-	
+
 	function delete_field($form_id, $name) {
 		$this->db->where('form_id', $form_id);
 		$this->db->where('name', $name);
@@ -105,24 +105,24 @@ class Form_model extends CI_Model {
 		));
 		#$this->db->delete('form_fields');
 	}
-	
+
 	function update_field($form_field_id, $data) {
 		$this->db->where('form_field_id', $form_field_id);
 		$this->db->update('form_fields', $data);
 	}
-	
+
 	function add_applicant($data) {
 		$this->db->insert('form_applicants', $data);
 		return $this->db->insert_id();
 	}
-	
+
 	function add_applicant_data($data) {
 		$this->db->insert('form_applicant_data', $data);
 		return $this->db->insert_id();
 	}
-	
+
 	function get_applicants() {
-		$sql = "SELECT a.*, f.*, count(a.applicant_id) as total_fields 
+		$sql = "SELECT a.*, f.*, count(a.applicant_id) as total_fields
 				FROM form_applicants a, form_applicant_data d, forms f
 				WHERE d.applicant_id = a.applicant_id
 				AND f.form_id = a.form_id
@@ -133,7 +133,7 @@ class Form_model extends CI_Model {
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
-	
+
 	function get_applicant_name($applicant_id)
 	{
 		$sql = "SELECT f.form_id, f.label, f.name, d.value
@@ -147,7 +147,7 @@ class Form_model extends CI_Model {
 		{
 			$name = $result['value'];
 		}
-		
+
 		$sql = "SELECT f.form_id, f.label, f.name, d.value
 				FROM form_applicant_data d, form_fields f
 				WHERE d.applicant_id = $applicant_id
@@ -160,7 +160,7 @@ class Form_model extends CI_Model {
 		}
 		return trim($name);
 	}
-	
+
 	function get_applicant($applicant_id) {
 		$sql = "SELECT f.form_id, f.label, f.name, d.value, c.type
 				FROM form_applicant_data d
@@ -171,7 +171,7 @@ class Form_model extends CI_Model {
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
-	
+
 	function reject_applicant($applicant_id) {
 		$this->db->where('applicant_id', $applicant_id);
 		return $this->db->update('form_applicants', array(
@@ -179,7 +179,7 @@ class Form_model extends CI_Model {
 			'rejected_on' => date('Y-m-d H:i:s')
 		));
 	}
-	
+
 	function accept_applicant($applicant_id) {
 		$this->db->where('applicant_id', $applicant_id);
 		return $this->db->update('form_applicants', array(
